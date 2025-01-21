4d6e0f108b79989deb40597c9b31e06efd69ed5f
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-01-21 13:09:26 +0530

    Added theory part in never_type.ts

--- typescript_training/Module_1_2/never_type.ts
+++ typescript_training/Module_1_2/never_type.ts
@@ -1,3 +1,5 @@
+//In TypeScript, the Never type is a function that never returns a value but throws an exception or error and terminates the program. It is often used to indicate that a function will not return a value or that a variable will never have a value. Here's a brief overview of never:
+
 let userInput: unknown;
 let userName: string;
 

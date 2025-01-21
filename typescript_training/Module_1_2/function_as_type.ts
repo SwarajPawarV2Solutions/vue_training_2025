a6f1983bd14ed2d2b62588621a29c3caaf6c5cb9
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-01-21 12:02:43 +0530

    Added function as type void.ts and function as type.ts file in typescript folder

---
+++ typescript_training/Module_1_2/function_as_type.ts
@@ -0,0 +1,15 @@
+function addition(n1: number, n2: number){
+    return n1 + n2
+}
+
+function printresult1(num: number){
+  console.log('Result: ' + num);
+}
+
+
+let combineValues: (a: number, b: number) => number; // we are setting type as a function so it will not accept string , number and boolean.
+
+combineValues = addition;
+
+console.log(combineValues(30,20));
+
--- typescript_training/Module_1_2/function_return_types_void.ts
+++ typescript_training/Module_1_2/function_return_types_void.ts
@@ -1,10 +1,10 @@
-function addition(n1: number, n2: number){
+function add(n1: number, n2: number){
     return n1 + n2
 }
 
 // Void type 
 
 function printresult(num: number){
-  console.log('Result: ' + num);
+  console.log('Result: ' +num );
+  
 }
-

a3ba2c31b2c5ce69dd3a2f3f7f3fdf13a190e375
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-01-30 17:36:45 +0530

    Adder Router code in router folder

---
+++ vue_training/Mini-Projects/router/router/src/views/PostDetailView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="posts">
+      <h1>This is a Post page</h1>
+      <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+      <div>
+        <button @click="showPostId">Show Post Id</button>
+      </div>
+      <div>
+        <button @click="goHomeIn3Seconds">Go Home in 3 Seconds</button>
+      </div>
+      <div>
+        <button @click="gotoFirstPost">Go to First Post</button>
+      </div>
+      <RouterLink to="/posts">&lt; Back</RouterLink>
+    </div>
+  </template>
+  
+  <script setup>
+
+import {useRoute, useRouter} from 'vue-router'
+
+const route = useRoute();
+const router = useRouter();
+
+  const showPostId = () => {
+    alert(` The Id of this post is: ${route.params.id}`)
+  }
+
+  const goHomeIn3Seconds = () => {
+    setTimeout(() => {
+       router.push('/')
+    }, 3000);
+  }
+
+  const gotoFirstPost = () => {
+     router.push({
+     name: 'postDetail',
+     params: {
+      id: 'id1'
+     }
+  })
+  }
+  </script>
+  

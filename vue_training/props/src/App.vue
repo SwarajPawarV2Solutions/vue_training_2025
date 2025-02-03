844d1ed0e55857e0d47af1970a81b7258cb1e167
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-02-03 12:53:22 +0530

    Added props

---
+++ vue_training/props/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+ 
+<div class="app">
+   <h1>Props in Vue.js</h1>
+   <Child v-bind:users='users'/>
+</div>
+
+</template>
+
+
+<script>
+import Child from './components/Child.vue'
+
+ export default {
+  name : 'App',
+  components : {
+    Child,
+  },
+  data(){
+   return  {users: [{name:'Swaraj',age:25,car:"Mercedes"},
+    {name:'Sourabh',age:27,car:"Audi"},
+    {name:'Abhishek',age:26,car:"porche"},
+    {name:'Shubham',age:28,car:"BMW"},
+   ]}
+  }
+ };
+ 
+</script>
+
+
+
+<style scoped>
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_training/props/src/components/Child.vue
@@ -0,0 +1,19 @@
+<!-- Child Component -->
+<template>
+    <div>
+        <h1>Child Component</h1>
+        <div v-for="user in users" :key="user.name">
+            <h2>{{ user.name }}</h2>
+            <h3>{{ user.age }}</h3>
+            <h3>{{ user.car }}</h3>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+     export default {
+        name : 'Child',
+        props: ['users' ]
+     }
+</script>

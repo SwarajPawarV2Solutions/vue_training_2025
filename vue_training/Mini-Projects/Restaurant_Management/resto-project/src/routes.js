17428e291474dcc005f1be8614beb3fc185724ff
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-02-04 22:44:54 +0530

    Added Add Restaurant Componennt and Update Restaurant component and also added routes for same and 1 feature of display user name on both pages has been added

--- vue_training/Mini-Projects/Restaurant_Management/resto-project/src/routes.js
+++ vue_training/Mini-Projects/Restaurant_Management/resto-project/src/routes.js
@@ -1,8 +1,9 @@
 import SignUp from "./components/SignUp.vue";
 import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
-
 import {createRouter, createWebHistory} from 'vue-router'
+import Update_Restaurant from "./components/Update_Restaurant.vue";
+import Add_Restaurant from "./components/Add_Restaurant.vue";
 
 const routes = [
     {
@@ -19,6 +20,16 @@ const routes = [
         name:'Login',
         component: Login,
         path:'/login'
+    },
+    {
+        name:'Update_Restaurant',
+        component: Update_Restaurant,
+        path:'/update-restaurant'
+    },
+    {
+        name:'Add_Restaurant',
+        component: Add_Restaurant,
+        path:'/add-restaurant'
     },
   
 

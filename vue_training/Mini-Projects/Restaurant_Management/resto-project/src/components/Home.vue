11339fec99efe7168c0c3de6ed4c532621fd1bda
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-02-05 00:01:38 +0530

    Added Restaurant Api in db.json and by using axios we access restaurent data on Add_restaurant page

--- vue_training/Mini-Projects/Restaurant_Management/resto-project/src/components/Home.vue
+++ vue_training/Mini-Projects/Restaurant_Management/resto-project/src/components/Home.vue
@@ -1,26 +1,53 @@
 <template>
   <Header/>
   <h1>Hello {{ name }}, Welcome on Home Page</h1>
+  <table border ="1px">
+    <tr>
+      <td>ID</td>
+      <td>Name</td>
+      <td>Address</td>
+      <td>Contact</td>
+    </tr>
+    <tr v-for="item in restaurant" :key="item.id">
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.address }}</td>
+      <td>{{ item.contact }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './Header.vue';
 export default {
   name: "Home",
   data(){
     return {
-      name:''
+      name:'',
+      restaurant:[],
     }
   },
   components:{
      Header
   },
-  mounted() {
+  async mounted() {
     let user = localStorage.getItem('user-info');
     this.name = JSON.parse(user).name
     if (!user) {
       this.$router.push({ name: 'SignUp' });
     }
-  },
+    let result = await axios.get("http://localhost:3000/restaurant");
+    console.log(result);
+    this.restaurant = result.data
+    
+  }
 };
 </script>
+
+<style>
+  td{
+    width: 160px;
+    height: 40px;
+  }
+</style>

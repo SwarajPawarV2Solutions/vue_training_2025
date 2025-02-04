739aa38b586b2b0c0f6cf1f9ff4977e9549f9642
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2025-02-04 15:29:10 +0530

    Added db.json file and from signUp() method we post data into db.json with help of Axios and also store data into localstorage

--- vue_training/Mini-Projects/Restaurant_Management_App/resto-project/src/components/SignUp.vue
+++ vue_training/Mini-Projects/Restaurant_Management_App/resto-project/src/components/SignUp.vue
@@ -2,17 +2,44 @@
 <img class="logo" src="../assets/resto_logo.png"/>
  <h1>Sign Up</h1>
  <div class="register">
-    <input type="text" placeholder="Enter Name"/>
-    <input type="text" placeholder="Enter Email"/>
-    <input type="password" placeholder="Enter Password"/>
-    <button>Sign Up</button>
+    <input type="text" v-model="name" placeholder="Enter Name"/>
+    <input type="text" v-model="email" placeholder="Enter Email"/>
+    <input type="password" v-model="password" placeholder="Enter Password"/>
+    <button v-on:click="signUp">Sign Up</button>
  </div>
 </template>
 
 <script>
-
+import axios from  'axios'
 export default {
-    name :'SignUp'
+    name :'SignUp',
+    data()
+    {
+      return {
+         name:'',
+         email:'',
+         password:''
+      }
+    },
+    methods:{
+       // signUp method will post data into db.json file
+     async signUp(){
+         console.log('signUp',this.name,this.email,this.password);
+         let result = await axios.post("http://localhost:3000/users",{
+            email:this.email,
+            name:this.name,
+            password:this.password
+         });
+         console.warn(result);
+         if(result.status==201)
+         {
+            alert("Sign Up Successful");
+         }
+         // To check user is already logged in or not by storing it in local Storage
+         
+        localStorage.setItem("user-info",JSON.stringify(result.data))
+      }
+    }
 }
 </script>
 
